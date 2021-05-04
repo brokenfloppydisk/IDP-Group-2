5b85decb6a13ae11fa1d475a27752e08f1d2bc44
brokenfloppydisk <kylehuang06@gmail.com>
2021-05-03 22:31:28 -0700

    Initial commit

---
+++ Assets/Main Menu/MainMenu.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenu : MonoBehaviour
+{
+    public string newGameScene;
+    public void NewGame(){
+        SceneManager.LoadScene(newGameScene);
+    }
+    public void QuitGame(){
+        Application.Quit();
+    }
+}
