0ee7afb5768d26a06d74d504db06ab3b50da5004
brokenfloppydisk <kylehuang06@gmail.com>
2021-05-11 00:21:34 -0700

    Created Tablet UI
    Created gameobjects and started setup, still need to add animations.

---
+++ Assets/Text/TextObject.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class TextObject
+{
+    public string title;
+    [TextArea(3,10)]
+    public string[] sentences;
+    
+}
