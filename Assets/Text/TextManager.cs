aee0cf7077136db196d013604ca47f119805b32e
brokenfloppydisk <kylehuang06@gmail.com>
2021-05-11 00:21:34 -0700

    Created Tablet UI
    Created gameobjects and started setup, still need to add animations.

---
+++ Assets/Text/TextManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class TextManager : MonoBehaviour
+{
+    private Queue<string> sentences;
+    public Text titleText;
+    public Text bodyText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+    public void StartText(TextObject textObj) {
+        titleText.text = textObj.title;
+        sentences.Clear();
+        foreach (string sentence in textObj.sentences) {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+    public void DisplayNextSentence() {
+        if (sentences.Count == 0) {
+            EndText();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        bodyText.text = sentence;
+    }
+    public void EndText() {
+        Debug.Log("End of Text");
+    }
+}
---
+++ Assets/Text/TextTrigger.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextTrigger : MonoBehaviour
+{
+    public TextObject textInterface = new TextObject();
+    public void TriggerText(){
+        FindObjectOfType<TextManager>().StartText(textInterface);
+    }
+    
+}
