6dccea5ae3bf5162f7808124e2b127a7de5740cf
brokenfloppydisk <kylehuang06@gmail.com>
2021-05-12 16:48:19 -0700

    Updated cipher scripts

--- Ciphers/bookcipher.py
+++ Ciphers/bookcipher.py
@@ -12,7 +12,3 @@ print(key)
 
 print("Decoding the book cipher gives:")
 print(ciphers.decode_book(book_content, key))
-
-encoded = ciphers.one_time_pad("This is a secret message from a caesar cipher.", 1, True, True)
-decoded = ciphers.one_time_pad(encoded, 1, False, True)
-print(decoded)
