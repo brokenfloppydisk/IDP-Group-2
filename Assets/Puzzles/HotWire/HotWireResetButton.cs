c48564c6c95bc3b19609f998f76943d06bb59c5b
brokenfloppydisk <kylehuang06@gmail.com>
2021-05-13 12:29:27 -0700

    Update Hotwire Puzzle

---
+++ Assets/Puzzles/HotWire/HotWireResetButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HotWireResetButton : MonoBehaviour
+{
+    public HotWirePuzzle puzzle;
+    
+    public void WireReset() {
+        puzzle.puzzleComplete = false;
+        for (int i = 0; i < puzzle.bottomWires.Count; i++) {
+            puzzle.bottomWires[i].success = false;
+            puzzle.topWires[i].success = false;
+        }
+
+    }
+}
