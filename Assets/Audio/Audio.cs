abb508e36327426aa51e42f44d8d6c10700922c9
brokenfloppydisk <kylehuang06@gmail.com>
2021-05-12 22:46:56 -0700

    Create Audio Script and Assets

---
+++ Assets/Audio/Audio.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Audio : MonoBehaviour
+{
+    
+    public AudioSource audioSource;
+
+    public AudioClip clip;
+
+    public float volume = 0.5f;
+
+    void playOneShot() {
+        audioSource.PlayOneShot(clip, volume);
+    }
+
+    void play() {
+        audioSource.Play();
+    }
+
+
+
+}
