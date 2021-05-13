3db713e8f2274b43726adb4928939bc0773af7a6
brokenfloppydisk <kylehuang06@gmail.com>
2021-05-13 01:07:06 -0700

    Started Hotwire Puzzle

---
+++ Assets/Puzzles/HotWire/HotWirePuzzle.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+[System.Serializable]
+public class HotWirePuzzle : MonoBehaviour {
+    public List<Wire> bottomWires = new List<Wire>();
+    public List<Wire> topWires = new List<Wire>();
+
+    
+}
