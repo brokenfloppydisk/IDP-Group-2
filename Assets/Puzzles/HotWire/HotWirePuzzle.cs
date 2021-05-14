fa171348f4cbc4c0ebf061a55cf0b3f9062d5b97
brokenfloppydisk <kylehuang06@gmail.com>
2021-05-13 22:05:47 -0700

    Finished Hotwire Puzzle
    Can also be reused for the match DNA base pairs puzzle

--- Assets/Puzzles/HotWire/HotWirePuzzle.cs
+++ Assets/Puzzles/HotWire/HotWirePuzzle.cs
@@ -4,26 +4,30 @@ using UnityEngine;
 using UnityEngine.UI;
 [System.Serializable]
 public class HotWirePuzzle : MonoBehaviour {
+    public List<Wire> nonHoveredWires = new List<Wire>();
     public List<Wire> bottomWires = new List<Wire>();
     public List<Wire> topWires = new List<Wire>();
-
     public bool puzzleComplete = false;
-
     public Wire currentDraggedWire;
     public Wire currentHoveredWire;
 
     private void Awake() {
         StartCoroutine(CheckCompletion());
     }
-    private IEnumerator CheckCompletion() {
+    private void Update() {
+
+    }
+
+    public IEnumerator CheckCompletion() {
         while (!puzzleComplete) {
             int successes = 0;
             for (int i = 0; i < topWires.Count; i++) {
                 if (topWires[i].success) { successes++; }
             }
             if (successes >= topWires.Count) {
-                Debug.Log("Success"); //Need to implement in another class
-                puzzleComplete = false;
+                Debug.Log("Success"); //Need to implement in playerstats class
+                puzzleComplete = true;
+                this.StopCoroutine(CheckCompletion());
             }
             yield return new WaitForSeconds(0.5f);
         }
