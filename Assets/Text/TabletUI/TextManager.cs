36e9841a23e5628f4395a9a4ef0045bcd4359fc4
brokenfloppydisk <kylehuang06@gmail.com>
2021-05-13 22:41:40 -0700

    Update Text Scripts

--- Assets/Text/TabletUI/TextManager.cs
+++ Assets/Text/TabletUI/TextManager.cs
@@ -7,8 +7,6 @@ public class TextManager : MonoBehaviour
     private Queue<string> sentences;
     public Text titleText;
     public Text bodyText;
-
-
     public List<Animator> animators;
 
     // Start is called before the first frame update
@@ -19,26 +17,30 @@ public class TextManager : MonoBehaviour
         for (int i = 0; i < animators.Count; i++) {
             animators[i].SetBool("TabletOpen", true);
         }
+        titleText.fontSize = textObj.titleFontSize;
+        titleText.color = textObj.textColor;
+        titleText.font = textObj.font;
+        bodyText.color = textObj.textColor;
+        bodyText.fontSize = textObj.fontSize;
+        bodyText.font = textObj.font;
         titleText.text = textObj.title;
         sentences.Clear();
         for (int i = 0; i < textObj.sentences.Length; i++) {
-            string sentence = textObj.sentences[i];
-            sentences.Enqueue(sentence);
+            string sentence_ = textObj.sentences[i];
+            sentences.Enqueue(sentence_);
         }
-
-        DisplayNextSentence();
+        next();
     }
-    public void DisplayNextSentence() {
+
+    public void next() {
         if (sentences.Count == 0) {
             EndText();
             return;
         }
-
         string sentence = sentences.Dequeue();
         StopAllCoroutines();
         StartCoroutine(TypeSentence(sentence));
     }
-
     IEnumerator TypeSentence(string sentence) {
         bodyText.text = "";
         char[] sentence_array = sentence.ToCharArray();
@@ -47,6 +49,7 @@ public class TextManager : MonoBehaviour
             bodyText.text += letter;
             yield return null;
         }
+        
     }
 
     public void EndText() {
