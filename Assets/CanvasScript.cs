23c18f10aba2393d42ec64aaeb2993dcbfff29a9
brokenfloppydisk <kylehuang06@gmail.com>
2021-05-18 00:22:07 -0700

    Started Implementing DNA Base Pairs Puzzle

---
+++ Assets/CanvasScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasScript : MonoBehaviour
+{
+    public Canvas canvas;
+    void Start()
+    {
+        canvas.worldCamera = FindObjectOfType<Camera>();
+    }
+}
