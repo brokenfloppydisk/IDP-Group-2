07c37af8f72e51fba5da43a69a7081a01ba01e72
brokenfloppydisk <kylehuang06@gmail.com>
2021-05-18 23:30:47 -0700

    Update DNA Puzzle

--- Assets/Images/Library/HologramFlashing.cs
+++ Assets/Images/Library/HologramFlashing.cs
@@ -4,16 +4,22 @@ using UnityEngine;
 using UnityEngine.UI;
 public class HologramFlashing : MonoBehaviour
 {
+    public float seconds;
     public Image image;
-    public Sprite[] sprites;
+    public Sprite[] spritesList;
     private int i = 1;
     private int cycles = 0;
     void Update()
     {
         if (cycles == 0) {
-            image.sprite = sprites[i % sprites.Length];
-            i++; 
-            cycles = Random.Range(0,119);
+            image.sprite = spritesList[i % 2];
+            if (i%2 == 1) {
+                cycles = 5;
+            } else {
+                cycles = Random.Range(0, (int) seconds*60-1);
+            }
+            i++;
+            
         }
         cycles--;
     }
