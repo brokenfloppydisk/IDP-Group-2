b3d2614d1833976d729860446595f446f9a5e5bc
brokenfloppydisk <kylehuang06@gmail.com>
2021-05-18 21:49:12 -0700

    Renamed Folders

---
+++ Assets/Images/Library/HologramFlashing.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class HologramFlashing : MonoBehaviour
+{
+    public Image image;
+    public Sprite[] sprites;
+    private int i = 1;
+    private int cycles = 0;
+    void Update()
+    {
+        if (cycles == 0) {
+            image.sprite = sprites[i % sprites.Length];
+            i++; 
+            cycles = Random.Range(0,119);
+        }
+        cycles--;
+    }
+}
