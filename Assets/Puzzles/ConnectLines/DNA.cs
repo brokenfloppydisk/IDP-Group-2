90254ea2e6926ee8c1465a4074d4874866163c2a
brokenfloppydisk <kylehuang06@gmail.com>
2021-05-19 00:30:35 -0700

    Finish DNA Puzzle

--- Assets/Puzzles/ConnectLines/DNA.cs
+++ Assets/Puzzles/ConnectLines/DNA.cs
@@ -1,19 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class DNA : HotWirePuzzle
 {
     public List<Animator> animators;
     public void openLockMenu() {
+        puzzleReset();
         setAnimationParam("PuzzleOpen", true);
+
     }
     public void closeLockMenu() {
+        clearLineRenderers();
         setAnimationParam("PuzzleOpen", false);
     }
     public void openLock() {
+        enterButton.interactable = false;
+        resetButton.interactable = false;
+        clearLineRenderers();
         StartCoroutine(moveLineRenderers());
         setAnimationParam("LockOpen", true);
+        
 
     }
     public void setAnimationParam(string param, bool value) {
@@ -28,18 +36,34 @@ public class DNA : HotWirePuzzle
                 if (topWires[i].success) { successes++; }
             }
             if (successes >= topWires.Count) {
-                Debug.Log("Success");
                 openLock();
                 puzzleComplete = true;
             }
         }
     }
     IEnumerator moveLineRenderers() {
-        for (int i=0; i <= 360; i++) {
-            foreach (Wire wire in bottomWires) {
-                wire.lineRenderer.SetPosition(0, transform.position);
-                wire.lineRenderer.SetPosition(1, topWires[wire.matchIndex].transform.position);
-                yield return new WaitForSeconds(0.2f);
+        for (int i=0; i <= 1; i++) {
+            List<Wire> possibleTopWires = topWires.ToList();
+            if (i==0) {
+                foreach (Wire wire in bottomWires) {
+                    wire.lineRenderer.SetPosition(0,Vector3.zero);
+                    wire.lineRenderer.SetPosition(1,Vector3.zero);
+                }
+                yield return new WaitForSeconds(0.45f);
+            } else {
+                foreach (Wire wire in bottomWires)
+                {
+                    wire.lineRenderer.SetPosition(0, wire.transform.position);
+                    foreach (Wire otherWire in possibleTopWires)
+                    {
+                        if (otherWire.matchIndex == wire.matchIndex)
+                        {
+                            possibleTopWires.Remove(otherWire);
+                            wire.lineRenderer.SetPosition(1, otherWire.transform.position);
+                            break;
+                        }
+                    }
+                }
             }
         }
     }
