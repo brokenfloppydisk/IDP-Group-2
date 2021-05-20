43044508a52a1fe35c992f21d2249f32ee681cc1
brokenfloppydisk <kylehuang06@gmail.com>
2021-05-20 13:41:53 -0700

    Implemented Cipher Guide "book"

---
+++ Assets/Text/TabletUI/Activator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Activator : Translator
+{
+    public GameObject activator;    
+    private TextTrigger textTrigger;
+    public bool usingTranslator;
+    public void Activate() {
+        if (usingTranslator) {
+            FindObjectOfType<CameraScript>().translateButton.SetActive(true);
+        }
+        else {
+            FindObjectOfType<CameraScript>().ciphersButton.SetActive(true);
+        }
+        textTrigger = GameObject.FindObjectOfType<TextTrigger>();
+        textTrigger.TriggerText();
+        activator.SetActive(false);
+    }
+}
---
+++ Assets/Text/TabletUI/CipherStartup.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CipherStartup : MonoBehaviour
+{
+    public GameObject ciphersButton;
+    void Awake() {
+        FindObjectOfType<CameraScript>().ciphersButton = this.ciphersButton;
+        ciphersButton.SetActive(false);
+    }
+}
