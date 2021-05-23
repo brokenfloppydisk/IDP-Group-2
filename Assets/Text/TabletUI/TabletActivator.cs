69043118784cd75c732a058f00b1d95685a8fe03
brokenfloppydisk <kylehuang06@gmail.com>
2021-05-23 16:39:35 -0700

    Made Tablet Acquirable
    In the lab room, the open tablet button now begins on the ground.

---
+++ Assets/Text/TabletUI/TabletActivator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class TabletActivator : MonoBehaviour
+{
+    public bool inGUI = false;
+    [SerializeField]
+    private Text text;
+    [SerializeField]
+    private Font newFont;
+    public void Click() {
+        if (!inGUI) {
+            StartCoroutine(changeTablet());
+            inGUI = true;
+        }
+    }
+    IEnumerator changeTablet() {
+        yield return new WaitForSeconds(1f);
+        text.text = "Tablet";
+        text.font = newFont;
+    } 
+}
