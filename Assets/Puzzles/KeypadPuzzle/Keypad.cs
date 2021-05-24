842d980d0357133cca02edbdb6c2bde05ffd5963
brokenfloppydisk <kylehuang06@gmail.com>
2021-05-23 23:53:37 -0700

    Finish entering coordinates puzzle

---
+++ Assets/Puzzles/KeypadPuzzle/Keypad.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Keypad : MonoBehaviour
+{
+    public KeypadPuzzle puzzle;
+    public int value = 0;
+    public Text outputText;
+    public Text sideText;
+    public string coordinateLabel;
+    public int index;
+    public Image correctLight;
+    public Button enterButton;
+    public Button resetButton;
+    public void Awake() {
+        puzzle = FindObjectOfType<KeypadPuzzle>();
+    }
+    public void Reset() {
+        value = 0;
+        puzzle.values[this.index] = 0;
+        outputText.text = "Z";
+    }
+    public void UpdateDisplay() {
+        if (puzzle.cameraScript.wiresConnected) {
+            puzzle.values[this.index] = value;
+            outputText.text = RomanNum.ToRoman(value);
+            sideText.text = coordinateLabel + "-COORD";
+        }
+        else {
+            value = 0;
+            puzzle.values[index] = 0;
+            outputText.text = "";
+            sideText.text = "";
+        }
+        
+    }
+    public void checkCorrect() {
+        if (puzzle.correctValues[this.index]==value) {
+            puzzle.successes[this.index] = true;
+            correctLight.sprite = puzzle.lightImages[1];
+            puzzle.keypadImages[this.index].sprite = puzzle.keypadSprites[1];
+            resetButton.interactable = false;
+            enterButton.interactable = false;
+        }
+        puzzle.checkCompletion();
+    }
+    public void close() {
+        puzzle.animator.SetBool("KeypadOpen", false);
+    }
+}
---
+++ Assets/Puzzles/KeypadPuzzle/KeypadPuzzle.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KeypadPuzzle : MonoBehaviour
+{
+    public Keypad keypad;
+    public List<int> values = new List<int>() {0,0,0};
+    public List<int> correctValues = new List<int>() {721,349,416};
+    public List<bool> successes = new List<bool>() {false,false,false};
+    public List<string> coordinates = new List<string>() {"X","Y","Z"};
+    public List<Image> keypadImages = new List<Image>();
+    public List<Sprite> keypadSprites = new List<Sprite>();
+    public Sprite[] lightImages;
+    [System.NonSerialized]
+    public CameraScript cameraScript;
+    public Animator animator;
+    public SceneChanger sceneChanger;
+    public void Awake() {
+        cameraScript = FindObjectOfType<CameraScript>();
+    }
+    public void checkCompletion() {
+        int _successes = 0;
+        for (int i = 0; i <successes.Count; i++) {
+            if (successes[i]) {
+                _successes++;
+            }
+        }
+        if (_successes==3) {
+            sceneChanger.NextScene();
+        }
+    }
+    
+}
