33521b0b01d3fb22df9bf3c683aa6974e6a7f9b0
brokenfloppydisk <kylehuang06@gmail.com>
2021-05-25 01:50:32 -0700

    Create Introduction Animation

---
+++ Assets/Text/Cutscenes/Intro/IntroCutscene.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class IntroCutscene : MonoBehaviour
+{
+    private int cutsceneStage = 0;
+    [SerializeField]
+    private Animator animator;
+    public void AdvanceCutscene() {
+        if (cutsceneStage < 7) {
+            cutsceneStage++;
+            animator.SetInteger("IntroSlideNum", cutsceneStage);
+        }
+        else {
+            SceneManager.LoadScene("ObjectLoader");
+        }
+    }
+}
