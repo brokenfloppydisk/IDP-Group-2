ba97c500d7b7e5b2a3e2fcc9d65ed484960a6a4c
Kyle Huang <72529534+brokenfloppydisk@users.noreply.github.com>
2021-06-02 15:00:49 -0700

    Update to Version 1.8

---
+++ Assets/Text/OpenLink.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenLink : MonoBehaviour
+{
+    public void OpenHyperlink(string link) {
+        Application.OpenURL(link);
+    }
+}
